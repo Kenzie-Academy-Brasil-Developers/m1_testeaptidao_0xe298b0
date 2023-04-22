05d79becfbd24d6b6f52af1e992416a6a9a447ae
0x298b0 <rodrigomfd@yahoo.com.br>
2023-04-22 19:05:43 -0300

    corrigido

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Variáveis para aplicar os resultados do teste. Você deverá categoriza-las como let/const, mas não deve alterar os valores.
-gender = process.argv[2]; // Genero 
-height = process.argv[3]; // Altura
-barReps = process.argv[4]; // Repetições com barra
-barSeconds = process.argv[5]; // Tempo das repetições com barra 
-abs = process.argv[6]; // Abdominais
-runDistance = process.argv[7]; // Distância da corrida
-runTime = process.argv[8]; // Tempo da corrida
-swimDistance = process.argv[9]; // Distância da natação
-swimTime = process.argv[10]; // Tempo da natação
-diveTime = process.argv[11]; // Tempo de mergulho
+let gender = process.argv[2]; // Genero 
+let height = process.argv[3]; // Altura
+let barReps = process.argv[4]; // Repetições com barra
+let barSeconds = process.argv[5]; // Tempo das repetições com barra 
+let abs = process.argv[6]; // Abdominais
+let runDistance = process.argv[7]; // Distância da corrida
+let runTime = process.argv[8]; // Tempo da corrida
+let swimDistance = process.argv[9]; // Distância da natação
+let swimTime = process.argv[10]; // Tempo da natação
+let diveTime = process.argv[11]; // Tempo de mergulho
 
-passed = false;
+let passed = false;
 
 /*
     Importante:
@@ -25,23 +25,29 @@ passed = false;
 //checar a altura, o número de repetições, abdominais, a distância corrida juntamente do tempo de corrida, ou a distância que foi percorrida na piscina juntamente do tempo do mergulho.
 
 if (gender === 'male') {
-    if (height >= 1.70) 
-    if ((barReps >= 6) || (barSeconds >= 15)) 
-    if (abs >= 41) 
-    if ((runDistance >= 3000) && (runTime <=720) || (runDistance >= 5000) && (runTime <=1200))
-    if ((swimDistance >= 100) && (swimTime <= 60) ||  (diveTime <= 30))
-    
-    passed = true
-}
-
-if (gender === 'female') {
-    if (height >= 1.60)
-    if ((barReps >= 5) || (barSeconds >= 12)) 
-    if (abs >= 41)  
-    if ((runDistance >= 4000) && (runTime <= 900) || (runDistance >= 6000) && (runTime <= 1320))  
-    if ((swimDistance >= 100) && (swimTime <= 60) ||  (diveTime <= 30))
-    
-    passed = true
+    if (height >= 1.70) {
+        if ((barReps >= 6) || (barSeconds >= 15)) {
+            if (abs >= 41) {
+                if ((runDistance >= 3000) && (runTime <=720) || (runDistance >= 5000) && (runTime <=1200)) {
+                    if ((swimDistance >= 100) && (swimTime <= 60) ||  (diveTime <= 30)) {
+                        passed = true
+                    }
+                } 
+            }
+        }
+    }
+} else if (gender === 'female') {
+    if (height >= 1.60) {
+        if ((barReps >= 5) || (barSeconds >= 12)) {
+            if (abs >= 41) {
+                if ((runDistance >= 4000) && (runTime <= 900) || (runDistance >= 6000) && (runTime <= 1320)) {
+                    if ((swimDistance >= 100) && (swimTime <= 60) ||  (diveTime <= 30)) {
+                        passed = true
+                    }
+                }
+            }
+        }
+    }
 }
 
 
